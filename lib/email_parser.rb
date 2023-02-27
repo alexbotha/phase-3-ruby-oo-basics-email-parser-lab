74b4c3db8d6c14aaadc1c3b0aa54f788136408d1
alexbotha <alexbotha@hotmail.co.uk>
2023-02-27 10:51:03 +0000

    commit tree b16cbcdcac75d05c75e4a02098b85b4b3ccf6396

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,43 @@
-# Build a class EmailAddressParser that accepts a string of unformatted 
-# emails. The parse method on the class should separate them into
-# unique email addresses. The delimiters to support are commas (',')
-# or whitespace (' ').
+# # Build a class EmailAddressParser that accepts a string of unformatted 
+# require 'pry'
+# class EmailAddressParser
+#   attr_reader :emails
+#   def initialize(emails)
+#     @emails = emails
+# end 
+
+# def parse
+#   #want to turn individual strings into the emails return in an array 
+#   xx = @emails
+#   new_string = xx.gsub(",", "")
+  
+#   new_string.split.uniq()
+# end
+# end 
+# # emails. The parse method on the class should separate them into
+# # unique email addresses. The delimiters to support are commas (',')
+# # or whitespace (' ').
+# my_string = "john@doe.com, person@somewhere.org"
+# #my_array = my_string.split
+# new_string = my_string.gsub(',', '')
+
+# binding.pry
+
+require 'pry'
+
+class EmailAddressParser
+
+  attr_reader :emails 
+
+  def initialize(emails)
+  @emails = emails
+  end 
+
+  def parse
+    x = @emails 
+    y = x.gsub(",", "")
+    k = y.split.uniq()
+    
+  end 
+  end 
+
